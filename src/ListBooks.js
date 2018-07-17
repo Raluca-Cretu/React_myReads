4e97707e8864b33ad89bf4d41bcf84cd895186e6
Raluca Cretu <ralucacr2000@yahoo.com>
2018-07-17 21:29:09 +0200

    Redo ListBooks.js render method

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,53 +1,25 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import * as BooksAPI from './BooksAPI';
+
 
 class ListBooks extends Component {
-	state = {
-		books: []
-	}
-
 	static propTypes = {
 		books: PropTypes.array.isRequired,
-		onAddBook: PropTypes.func.isRequired
 	}
 
-	renderShelf() {
+	render() {
 		return (
 	        <div className='bookshelf'>
 				<h2 className='bookshelf-title'>{title}</h2>
           		<div className='bookshelf-books'>
           			<ol className="books-grid">
-          				<li>
-          				{books.filter (book =>  book.shelf === "currently reading" }
-          				</li>
-          				<li>
-          				{books.filter (book =>  book.shelf === "want to read" }
-          				</li>
-          				<li>
-          				{books.filter (book =>  book.shelf === "read" }
-          				</li>
+        				{books.map(book => books)}
           			</ol>
           		</div>
           	</div>
 		)
 	}
 
-	render() {
-		return (
-			<div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-
-                </div>
-                <div className="open-search">
-                </div>
-            </div>
-		)
-
-	}
 }
 
 export default ListBooks
